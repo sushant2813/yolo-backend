ccd0512d95096f616896a4aa20a0c23cd89feab4
sushant2813 <153453603+sushant2813@users.noreply.github.com>
2025-02-09 12:56:04 +0530

    Add start.sh script to start Flask app

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment (if applicable)
+# source /path/to/your/virtualenv/bin/activate
+
+# Install required dependencies (you can also run this manually once)
+pip install -r requirements.txt
+
+# Run the Flask app
+python3 app.py
